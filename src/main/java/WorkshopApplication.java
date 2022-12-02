9ea33b69d35da84ba518f8f5e5d9cf8a95b35e7d
camlo01 <camilobeltran174@gmail.com>
2022-12-02 14:40:17 -0500

    Removed code

--- src/main/java/WorkshopApplication.java
+++ src/main/java/WorkshopApplication.java
@@ -14,30 +14,6 @@ public class WorkshopApplication {
     public static void main(String[] args) {
 
         initializedSuccessfully();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-
-        ArrayList<Fruit> fruits = new ArrayList<>();
-
-        Fruit fruitReusable = new Fruit("Sandía", "Citrullus lanatus", 3000, 1450, new ArrayList<>()
-                , new ArrayList<>(), 2.000F, 30, true, false, Fruit.Maturity.MATURITY, Fruit.Taste.SWEET);
-
-        fruits.add(fruitReusable);
-        fruits.add(fruitReusable);
-        fruits.add(fruitReusable);
-
-        Invoice invoice = new Invoice(1, "Camilo", "Matt", fruits);
-
-        System.out.println(invoice.showInvoice());
-
-
-
-
 
     }
 
@@ -49,7 +25,5 @@ public class WorkshopApplication {
         System.out.println("Initialized Successfully!");
         System.out.println("-------------------------");
     }
-
-
 
 }
